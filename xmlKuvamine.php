bf34db4d34a7ec53b240036f21103e71378bfd65
danil adamiv <adamivdanil@gmail.com>
2026-01-13 15:05:50 +0200

    Otsingu funktsioon lisadud ja töötab, lihtsalt pead vajutama enter klaviatuuril mitte ok nupp ekraanil, see x põhjusel katki.

--- xmlKuvamine.php
+++ xmlKuvamine.php
@@ -1,18 +1,47 @@
 <?php
 $opilased=simplexml_load_file("opilased.xml");
+$feed=simplexml_load_file("https://www.err.ee/rss/");
+function erialaOtsing($paring){
+    global $opilased;
+    $tulemus=array();
+    foreach($opilased->opilane as $opilane)
+    {
+        if(substr(strtolower($opilane->eriala),0,strlen($paring)) == strtolower($paring))
+        {
+            array_push($tulemus, $opilane);
+        }
+    }
+    return $tulemus;
+}
 ?>
 <!DOCTYPE html>
 <html>
 <head>
     <title>Xml faili kuvamine Opilased.xml</title>
 </head>
+<body>
 <h1>XML faili kuvamine - Opilased.XML</h1>
 <?php
 //1. õpilase nimi
 echo "1.õpilase nimi: ".$opilased->opilane[0]->nimi;
 
 ?>
-</html>
+<form action="?" method="post">
+    <label for="otsing">Eriala</label>
+    <input type="text" name="otsing" id="otsing">
+    <input type="submit" value="Ok"
+</form>
+<?php
+//otsingu tulemus
+if(!empty($_POST['otsing']))
+{
+    $tulemus=erialaOtsing($_POST['otsing']);
+    foreach($tulemus as $opilane) {
+        echo $opilane->nimi . " - " .$opilane->eriala."<br>";
+    }
+}
+?>
+
 <table>
     <tr>
         <th>Õpilase nimi</th>
@@ -33,3 +62,6 @@ echo "1.õpilase nimi: ".$opilased->opilane[0]->nimi;
 
     ?>
 </table>
+
+</body>
+</html>
